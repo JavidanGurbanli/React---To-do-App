7ec77dd3e632408fa6b4acd7f455f75e704cb299
Cavidan Qurbanlı <82161220+JavidanGurbanli@users.noreply.github.com>
2022-07-07 18:02:43 +0400

    solo 1 project

---
+++ src/components/About.js
@@ -0,0 +1,22 @@
+import react from 'react';
+import '../index.css';
+const About = () => {
+    return (
+        <div class="all">
+             <div className="About">
+               <h3 className="Title">About</h3>
+                <p>
+                I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.
+                </p>
+             </div>
+            <div className="Interests">
+              <h3 className="Title">Interests</h3>
+                <p>
+                Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.
+              </p>
+            </div>
+        </div>
+       
+    )
+}
+export default About;
